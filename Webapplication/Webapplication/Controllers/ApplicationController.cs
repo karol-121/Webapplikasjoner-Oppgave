e76ce402795dffdddd066bbfa9c9be3663fb2911
karol-121 <36259145+karol-121@users.noreply.github.com>
2021-09-27 13:59:23 +0200

    mer dokumentasjon og modifisering av logging informasjon

--- Webapplication/Webapplication/Controllers/ApplicationController.cs
+++ Webapplication/Webapplication/Controllers/ApplicationController.cs
@@ -14,23 +14,29 @@ namespace Webapplication.Controllers
     [Route("API/[action]")]
     public class ApplicationController : ControllerBase
     {
-        private readonly IApplicationRepository _Local_DB;
+        private readonly IApplicationRepository _Local_DB; //database objekt
 
-        private ILogger<ApplicationController> _Local_Log;
-
+        private ILogger<ApplicationController> _Local_Log; //log objekt
+        
         public ApplicationController(IApplicationRepository applicationRepository, ILogger<ApplicationController> logger)
         {
             _Local_DB = applicationRepository;
             _Local_Log = logger;
         }
 
+        //summary: henter alle ruter som finnes
+        //returns: liste med rute objekter
         public async Task<ActionResult> GetRoutes()
         {
             var Routes = await _Local_DB.GetRoutes(); //henter alle ruter som finnes i databasen
-            _Local_Log.LogInformation("Requested routes");
-            return Ok(Routes);
+            _Local_Log.LogInformation("Requested routes."); //logge informasjon om forespørsel
+            return Ok(Routes); //returnere liste med http ok response status
         }
 
+        //summary: henter alle samsvarende departures bestemt av parameters
+        //parameters: int Route - bestemmer ruten til departures, string From - dato string i format yyyy-MM-dd som bestemmer fra og med dato,
+        //string To - dato string i format yyyy-MM-dd som bestemmer til og med dato, int Passengers - antall personer som det skal være plass for
+        //returns: liste med samsvarende departure objekter, http Bad request - dersom parameter/parameterene er invalid
         public async Task<ActionResult> GetDepartures(int Route, string From, string To, int Passengers) 
         {
             try
@@ -38,23 +44,23 @@ namespace Webapplication.Controllers
                 var From_Date = DateTime.ParseExact(From, "yyyy-MM-dd", CultureInfo.InvariantCulture); //lager datetime objekt fra string parameter
                 var To_Date = DateTime.ParseExact(To, "yyyy-MM-dd", CultureInfo.InvariantCulture); //lager datetime objekt fra string parameter
 
-                Console.WriteLine(From_Date);
-                Console.WriteLine(To_Date);
-
                 var Departures = await _Local_DB.GetDepartures(Route, From_Date, To_Date); //henter alle utreiser i gitt intervall 
                 var AvailableDep = await _Local_DB.CheckAvailability(Departures, Passengers); //filtrerer og returnerer kun tilgjenglige utreiser
                 
-                _Local_Log.LogInformation("Requested departures");
-                return Ok(AvailableDep); 
+                _Local_Log.LogInformation("Requested departures for route "+Route+", from "+From_Date+" to "+To_Date+" for "+Passengers+" passenger(s)."); //logge informasjon om forespørsel
+                return Ok(AvailableDep); //returnere liste med http ok response status
             } 
             catch (Exception e)
             {
-                _Local_Log.LogError(e.Message);
-                return BadRequest(e.Message);
+                _Local_Log.LogError("Exception thrown while requesting departures: "+e.Message); //logge feilmelding
+                return BadRequest(e.Message); //returnere en http bad request response
             }
             
         }
 
+        //summary: validerer informasjon og registrerer ordre
+        //parameters: OrderInformation OrderInformation - objekt som holder informasjon om ordre
+        //returns: http Ok - ved vellykket registrering, http Bad request - dersom informasjon er invalid
         public async Task<ActionResult> RegisterOrder(OrderInformation OrderInformation)
         {
             if (!ModelState.IsValid) //sjekkes om data send inn har riktig format som er definert i modellen.
@@ -66,13 +72,12 @@ namespace Webapplication.Controllers
             try
             {
                 await _Local_DB.RegisterOrder(OrderInformation); //prøve å registrere nye ordre
-                _Local_Log.LogInformation("Order has been registered");
+                _Local_Log.LogInformation("Order has been registered."); //logger informasjon om vellykket registrering
                 return Ok("Ticket has been registered"); //returnere en ok http response status
             } 
             catch (Exception e)
             {
-                // dersom det er noe feil ved registrering, kastes det exception som fanges her.
-                _Local_Log.LogError(e.Message);
+                _Local_Log.LogError("Exception thrown while order registration: "+e.Message); //logge feilmelding
                 return BadRequest(e.Message); // returnere en error http response med excepton melding.
             }
         }
