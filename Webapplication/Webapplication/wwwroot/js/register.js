12a7f5121b79b3bb1e589d70a73a2529ea010611
karol-121 <36259145+karol-121@users.noreply.github.com>
2021-09-27 19:40:12 +0200

    lagt til en prototype løsning for registrering av ordrer
    dette er sikker noe som skal gjenbrukes i final løsning.

---
+++ Webapplication/Webapplication/wwwroot/js/register.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    //dette er jquery funksjonen som kjøres med en gang siden loader, holder den for å ikke glemme syntaksen
+});
+
+
+function registerOrder() {
+
+    const OrderInformation = {
+        Name: $("#name").val(),
+        Surname: $("#surname").val(),
+        Age: $("#age").val(),
+        Address: $("#address").val(),
+        Zip_Code: $("#zip").val(),
+        City: $("#city").val(),
+        Phone: $("#phone").val(),
+        Email: $("#email").val(),
+        Departure_Id: $("#dep").val(),
+        Passengers: $("#adults").val(),
+        Passengers_Underage: $("#underage").val(),
+        Pets: $("#pets").val(),
+        Vehicles: $("#vehicles").val()
+    }
+
+    const url = "API/RegisterOrder";
+    $.post(url, OrderInformation).done(function () {
+        alert("nice one!")
+    }).fail(function () {
+        alert("error!")
+    });
+
+}
+
+
+
+
+
+
+
+
