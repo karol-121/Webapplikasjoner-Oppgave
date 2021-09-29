2e60a70adc09855701778a96930a999635123dcc
karol-121 <36259145+karol-121@users.noreply.github.com>
2021-09-29 19:30:51 +0200

    det hentes og vises to tabeller for tur og retur

--- Webapplication/Webapplication/wwwroot/js/view.js
+++ Webapplication/Webapplication/wwwroot/js/view.js
@@ -66,26 +66,29 @@ function show() {
     const dateReturn = $("#dateReturn").val();
     const passengers = $("#passengers").val();
 
+
     //input validation goes here i guess, just do not verify the date return and route reverse if one way is choosen
     //question is if the date should be validated, as currently it will default to todays date anyway.
     //could be also nice if route reverse if wrong, do not allow for two way orders
 
+    //also check if the return date is greater than leave, otherwise it is a error as you can not return before you go.
+
     const dateIntervalLeave = new DateInterval(DateUtilities.inputToDateObject(dateLeave));
     const dateIntervalReturn = new DateInterval(DateUtilities.inputToDateObject(dateReturn));
 
-    DeparturesLeave = fetchDepartures(route, dateIntervalLeave, passengers);
-
+    fetchDepartures(route, dateIntervalLeave, passengers, processLeaveDepartures);
 
     if (TourType == 1) {
         //if both tours are choosed, fetch also the returns
-        DeparturesReturn = fetchDepartures(routeReverse, dateIntervalReturn, passengers);
+        fetchDepartures(routeReverse, dateIntervalReturn, passengers, processReturnDepartures);
     }
 
 }
 
 //summary: lager spørring og henter data fra serveren
-//parameters: route - id til route, from - Date objekt fra dato, to - Date objekt til dato, passengers - antall personer
-function fetchDepartures(route, dateInterval, passengers) {
+//parameters: route - id til route, from - Date objekt fra dato, to - Date objekt til dato, passengers - antall personer, 
+//dataProcessingFunction - funksjonen hvor resultat data vil bli sendt til for videre prosessering.
+function fetchDepartures(route, dateInterval, passengers, dataProcessingFunction) {
 
     const from = dateInterval.getStartInterval();
     const to = dateInterval.getEndInterval();
@@ -96,28 +99,45 @@ function fetchDepartures(route, dateInterval, passengers) {
 
     $.get(url, function (data) {
         console.log("departure fetch ok");
-
-        return data;
+        dataProcessingFunction(data);
 
     }).fail(function () {
         console.log("departure fetch failed!");
-        return null;
         //her skal det vises noe alert tingy.
 
     });
-
 }
 
-function displayData(Departures, dateInterval) {
-    //i do like it to populate both tables where it is possible
+function processLeaveDepartures(departures) {
+    DeparturesLeave = departures;
 
-    const header = "Utreise (" + DateUtilities.toLocalDateString(dateInterval.getStartInterval()) + " - " + DateUtilities.toLocalDateString(dateInterval.getEndInterval()) + ")";
-    $("#timetable-header").html(header);
+    displayDepartures(DeparturesLeave, $("#timetable-leave-title"), $("#timetable-leave-header"), $("#timetable-leave-body"));
+    //print Departures Leave
+}
 
-    let tableContent = "<tr><th>dato:</th><th>pris</th><th></th></tr>";
-    for (let t of Departures) {
-        tableContent += "<tr><td>" + DateUtilities.isoToLocalDateString(t.date) + "</td><td>" + t.cruise.passeger_Price + "</td><td><a href='index.html'> velg</a></td</tr>";
+function processReturnDepartures(departures) {
+    DeparturesReturn = departures;
+
+    displayDepartures(DeparturesReturn, $("#timetable-return-title"), $("#timetable-return-header"), $("#timetable-return-body"));
+    //print Departures Return
+}
+
+
+
+
+
+function displayDepartures(items, DOM_title, DOM_tableHeader, DOM_tableBody) {
+
+    //const title = "Utreise (" + DateUtilities.toLocalDateString(dateInterval.getStartInterval()) + " - " + DateUtilities.toLocalDateString(dateInterval.getEndInterval()) + ")";
+    DOM_title.html("boi");
+
+    const header = "<tr><th>dato:</th><th>pris:</th></tr>";
+    DOM_tableHeader.html(header);
+
+    let tableContent = new String();
+    for (let i of items) {
+        tableContent += "<tr><td>" + DateUtilities.isoToLocalDateString(i.date) + "</td><td>" + i.cruise.passeger_Price + "</td></tr>";
     }
-    $("#timetable").html(tableContent);
+    DOM_tableBody.html(tableContent);
 
 }
