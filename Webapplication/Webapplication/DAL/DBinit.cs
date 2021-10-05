d02bea930da5400ce8de405053d4a3bc00424522
karol-121 <36259145+karol-121@users.noreply.github.com>
2021-10-05 16:45:52 +0200

    lagt til andre uke med departures inn i dbinit

--- Webapplication/Webapplication/DAL/DBinit.cs
+++ Webapplication/Webapplication/DAL/DBinit.cs
@@ -33,28 +33,41 @@ namespace Webapplication.DAL
 
                 //Cruise1
                 var week1_mon_hour8 = currentDate.AddDays(1).AddHours(8);
+                var week2_mon_hour8 = currentDate.AddDays(8).AddHours(8);
 
                 var week1_mon_hour17 = currentDate.AddDays(1).AddHours(17);
+                var week2_mon_hour17 = currentDate.AddDays(8).AddHours(17);
 
                 var week1_wen_hour8 = currentDate.AddDays(3).AddHours(8);
+                var week2_wen_hour8 = currentDate.AddDays(10).AddHours(8);
 
                 var week1_wen_hour17 = currentDate.AddDays(3).AddHours(17);
+                var week2_wen_hour17 = currentDate.AddDays(10).AddHours(17);
 
                 var week1_fri_hour8 = currentDate.AddDays(5).AddHours(8);
+                var week2_fri_hour8 = currentDate.AddDays(12).AddHours(8);
 
                 var week1_fri_hour17 = currentDate.AddDays(5).AddHours(17);
+                var week2_fri_hour17 = currentDate.AddDays(12).AddHours(17);
 
                 //Cruise2
                 var week1_tue_hour8 = currentDate.AddDays(2).AddHours(8);
+                var week2_tue_hour8 = currentDate.AddDays(9).AddHours(8);
 
                 var week1_tue_hour15 = currentDate.AddDays(2).AddHours(15);
+                var week2_tue_hour15 = currentDate.AddDays(9).AddHours(15);
 
                 var week1_thu_hour8 = currentDate.AddDays(4).AddHours(8);
+                var week2_thu_hour8 = currentDate.AddDays(11).AddHours(8);
 
                 var week1_thu_hour15 = currentDate.AddDays(4).AddHours(15);
+                var week2_thu_hour15 = currentDate.AddDays(11).AddHours(15);
 
                 //Cruise3
                 var week1_sat_hour10 = currentDate.AddDays(6).AddHours(10);
+                var week1_sun_hour10 = currentDate.AddDays(7).AddHours(10);
+                var week2_sat_hour10 = currentDate.AddDays(13).AddHours(10);
+                var week2_sun_hour10 = currentDate.AddDays(14).AddHours(10);
 
                 //Routes
                 var san_str = new Route { Origin = "Sandefjord", Destination = "Strømstad", Return_id = 2 };
@@ -80,7 +93,7 @@ namespace Webapplication.DAL
                 var Cruise3_rev = new Cruise { Route = kie_osl, CruiseDetails = cruise3_details };
 
     
-                //Cruise1
+                //Cruise1 uke 1
                 var Schedule1 = new Departure { Cruise = Cruise1, Date = week1_mon_hour8 };
                 var Schedule2 = new Departure { Cruise = Cruise1, Date = week1_mon_hour17 };
                 var Schedule3 = new Departure { Cruise = Cruise1, Date = week1_wen_hour8 };
@@ -88,54 +101,85 @@ namespace Webapplication.DAL
                 var Schedule5 = new Departure { Cruise = Cruise1, Date = week1_fri_hour8 };
                 var Schedule6 = new Departure { Cruise = Cruise1, Date = week1_fri_hour17 };
 
-                //Cruise1 reverse
-                var Schedule7 = new Departure { Cruise = Cruise1_rev, Date = week1_mon_hour8 };
-                var Schedule8 = new Departure { Cruise = Cruise1_rev, Date = week1_mon_hour17 };
-                var Schedule9 = new Departure { Cruise = Cruise1_rev, Date = week1_wen_hour8 };
-                var Schedule10 = new Departure { Cruise = Cruise1_rev, Date = week1_wen_hour17 };
-                var Schedule11 = new Departure { Cruise = Cruise1_rev, Date = week1_fri_hour8 };
-                var Schedule12 = new Departure { Cruise = Cruise1_rev, Date = week1_fri_hour17 };
+                //Cruise1 uke 2
+                var Schedule7 = new Departure { Cruise = Cruise1, Date = week2_mon_hour8 };
+                var Schedule8 = new Departure { Cruise = Cruise1, Date = week2_mon_hour17 };
+                var Schedule9 = new Departure { Cruise = Cruise1, Date = week2_wen_hour8 };
+                var Schedule10 = new Departure { Cruise = Cruise1, Date = week2_wen_hour17 };
+                var Schedule11 = new Departure { Cruise = Cruise1, Date = week2_fri_hour8 };
+                var Schedule12 = new Departure { Cruise = Cruise1, Date = week2_fri_hour17 };
 
-                //Cruise2
-                var Schedule13 = new Departure { Cruise = Cruise2, Date = week1_tue_hour8 };
-                var Schedule14 = new Departure { Cruise = Cruise2, Date = week1_tue_hour15 };
-                var Schedule15 = new Departure { Cruise = Cruise2, Date = week1_thu_hour8 };
-                var Schedule16 = new Departure { Cruise = Cruise2, Date = week1_thu_hour15 };
+                //Cruise1 reverse uke 1
+                var Schedule13 = new Departure { Cruise = Cruise1_rev, Date = week1_mon_hour8 };
+                var Schedule14 = new Departure { Cruise = Cruise1_rev, Date = week1_mon_hour17 };
+                var Schedule15 = new Departure { Cruise = Cruise1_rev, Date = week1_wen_hour8 };
+                var Schedule16 = new Departure { Cruise = Cruise1_rev, Date = week1_wen_hour17 };
+                var Schedule17 = new Departure { Cruise = Cruise1_rev, Date = week1_fri_hour8 };
+                var Schedule18 = new Departure { Cruise = Cruise1_rev, Date = week1_fri_hour17 };
 
-                //Cruisea reverse
-                var Schedule17 = new Departure { Cruise = Cruise2_rev, Date = week1_tue_hour8 };
-                var Schedule18 = new Departure { Cruise = Cruise2_rev, Date = week1_tue_hour15 };
-                var Schedule19 = new Departure { Cruise = Cruise2_rev, Date = week1_thu_hour8 };
-                var Schedule20 = new Departure { Cruise = Cruise2_rev, Date = week1_thu_hour15 };
+                //Cruise1 reverse uke 2
+                var Schedule19 = new Departure { Cruise = Cruise1_rev, Date = week2_mon_hour8 };
+                var Schedule20 = new Departure { Cruise = Cruise1_rev, Date = week2_mon_hour17 };
+                var Schedule21 = new Departure { Cruise = Cruise1_rev, Date = week2_wen_hour8 };
+                var Schedule22 = new Departure { Cruise = Cruise1_rev, Date = week2_wen_hour17 };
+                var Schedule23 = new Departure { Cruise = Cruise1_rev, Date = week2_fri_hour8 };
+                var Schedule24 = new Departure { Cruise = Cruise1_rev, Date = week2_fri_hour17 };
 
-                //Cruise3
-                var Schedule21 = new Departure { Cruise = Cruise3, Date = week1_sat_hour10 };
+                //Cruise2 uke 1
+                var Schedule25 = new Departure { Cruise = Cruise2, Date = week1_tue_hour8 };
+                var Schedule26 = new Departure { Cruise = Cruise2, Date = week1_tue_hour15 };
+                var Schedule27 = new Departure { Cruise = Cruise2, Date = week1_thu_hour8 };
+                var Schedule28 = new Departure { Cruise = Cruise2, Date = week1_thu_hour15 };
 
-                //Cruise3 reverse
-                var Schedule22 = new Departure { Cruise = Cruise3_rev, Date = week1_sat_hour10 };
+                //Cruise2 uke 2
+                var Schedule29 = new Departure { Cruise = Cruise2, Date = week2_tue_hour8 };
+                var Schedule30 = new Departure { Cruise = Cruise2, Date = week2_tue_hour15 };
+                var Schedule31 = new Departure { Cruise = Cruise2, Date = week2_thu_hour8 };
+                var Schedule32 = new Departure { Cruise = Cruise2, Date = week2_thu_hour15 };
+
+                //Cruise2 reverse uke 1
+                var Schedule33 = new Departure { Cruise = Cruise2_rev, Date = week1_tue_hour8 };
+                var Schedule34 = new Departure { Cruise = Cruise2_rev, Date = week1_tue_hour15 };
+                var Schedule35 = new Departure { Cruise = Cruise2_rev, Date = week1_thu_hour8 };
+                var Schedule36 = new Departure { Cruise = Cruise2_rev, Date = week1_thu_hour15 };
+
+                //Cruise2 reverse uke 2
+                var Schedule37 = new Departure { Cruise = Cruise2_rev, Date = week2_tue_hour8 };
+                var Schedule38 = new Departure { Cruise = Cruise2_rev, Date = week2_tue_hour15 };
+                var Schedule39 = new Departure { Cruise = Cruise2_rev, Date = week2_thu_hour8 };
+                var Schedule40 = new Departure { Cruise = Cruise2_rev, Date = week2_thu_hour15 };
+
+                //Cruise3 (reverse) uke 1
+                var Schedule41 = new Departure { Cruise = Cruise3, Date = week1_sat_hour10 };
+                var Schedule42 = new Departure { Cruise = Cruise3_rev, Date = week1_sun_hour10 };
+
+                //Cruise3 (reverse) uke 2
+                var Schedule43 = new Departure { Cruise = Cruise3, Date = week2_sat_hour10 };
+                var Schedule44 = new Departure { Cruise = Cruise3_rev, Date = week2_sun_hour10 };
+                
 
 
                 //routes som skal inn i databasen
 
-                Context.Routes.Add(san_str);
+                /*Context.Routes.Add(san_str);
                 Context.Routes.Add(str_san);
                 Context.Routes.Add(stav_berg);
                 Context.Routes.Add(berg_stav);
                 Context.Routes.Add(osl_kie);
-                Context.Routes.Add(kie_osl);
+                Context.Routes.Add(kie_osl);*/
 
                 //cruises som skal inn i databasen
 
-                Context.Cruises.Add(Cruise1);
+                /*Context.Cruises.Add(Cruise1);
                 Context.Cruises.Add(Cruise1_rev);
                 Context.Cruises.Add(Cruise2);
                 Context.Cruises.Add(Cruise2_rev);
                 Context.Cruises.Add(Cruise3);
-                Context.Cruises.Add(Cruise3_rev);
+                Context.Cruises.Add(Cruise3_rev);*/
 
-                //departures skal inn i databasen
+                //departures som skal inn i databasen
 
-                //cruise 1
+                //cruise 1 uke 1
                 Context.Departures.Add(Schedule1);
                 Context.Departures.Add(Schedule2);
                 Context.Departures.Add(Schedule3);
@@ -143,7 +187,7 @@ namespace Webapplication.DAL
                 Context.Departures.Add(Schedule5);
                 Context.Departures.Add(Schedule6);
 
-                //cruise 1 reverse
+                //cruise 1 uke 2
                 Context.Departures.Add(Schedule7);
                 Context.Departures.Add(Schedule8);
                 Context.Departures.Add(Schedule9);
@@ -151,24 +195,53 @@ namespace Webapplication.DAL
                 Context.Departures.Add(Schedule11);
                 Context.Departures.Add(Schedule12);
 
-                //cruise 2
+                //cruise 1 rev uke 1
                 Context.Departures.Add(Schedule13);
                 Context.Departures.Add(Schedule14);
                 Context.Departures.Add(Schedule15);
                 Context.Departures.Add(Schedule16);
-
-                //cruise 2 reverse
                 Context.Departures.Add(Schedule17);
                 Context.Departures.Add(Schedule18);
+
+                //cruise 1 rev uke 2
                 Context.Departures.Add(Schedule19);
                 Context.Departures.Add(Schedule20);
-
-                //cruise 3 
                 Context.Departures.Add(Schedule21);
-
-                //cruise 3 reverse
                 Context.Departures.Add(Schedule22);
+                Context.Departures.Add(Schedule23);
+                Context.Departures.Add(Schedule24);
 
+                //cruise 2 uke 1
+                Context.Departures.Add(Schedule25);
+                Context.Departures.Add(Schedule26);
+                Context.Departures.Add(Schedule27);
+                Context.Departures.Add(Schedule28);
+
+                //cruise 2 uke 2
+                Context.Departures.Add(Schedule29);
+                Context.Departures.Add(Schedule30);
+                Context.Departures.Add(Schedule31);
+                Context.Departures.Add(Schedule32);
+
+                //cruise 2 rev uke 1
+                Context.Departures.Add(Schedule33);
+                Context.Departures.Add(Schedule34);
+                Context.Departures.Add(Schedule35);
+                Context.Departures.Add(Schedule36);
+
+                //cruise 2 rev uke 2
+                Context.Departures.Add(Schedule37);
+                Context.Departures.Add(Schedule38);
+                Context.Departures.Add(Schedule39);
+                Context.Departures.Add(Schedule40);
+
+                //cruise 3 (rev) uke 1
+                Context.Departures.Add(Schedule41);
+                Context.Departures.Add(Schedule42);
+
+                //cruise 3 (rev) uke 2
+                Context.Departures.Add(Schedule43);
+                Context.Departures.Add(Schedule44);
 
                 Context.SaveChanges();
             }
